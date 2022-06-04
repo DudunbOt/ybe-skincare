a57735e3a9951b1590436f3f5cd36c43d1ac00d0
Pineda Prima Yoga <pinedaprima12@gmail.com>
2022-06-04 19:48:19 +0700

    create models for products

---
+++ common/models/Products.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $descriptions
+ * @property string|null $image
+ * @property float $price
+ * @property int $status
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price', 'status'], 'required'],
+            [['descriptions'], 'string'],
+            [['price'], 'number'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['image'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'descriptions' => 'Descriptions',
+            'image' => 'Image',
+            'price' => 'Price',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\ProductsQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\ProductsQuery(get_called_class());
+    }
+}
